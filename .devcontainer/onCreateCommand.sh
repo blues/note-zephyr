fa95c1a7214b1ee314982a6ad4b55a5e6e86bd35
Alex Bucknall <alex.bucknall@gmail.com>
2025-03-04 13:20:31 +0000

    fix: updates build settings

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -8,4 +8,5 @@ pip install -r /workdir/zephyr/scripts/requirements.txt --root-user-action
 echo "alias ll='ls -lah'" >> $HOME/.bashrc
 west completion bash > $HOME/west-completion.bash
 echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
+echo "PATH=/opt/toolchains/zephyr-sdk-${ZSDK_VERSION}/sysroots/$(uname -m)-pokysdk-linux/usr/bin:\$PATH" >> $HOME/.bashrc
 history -c
